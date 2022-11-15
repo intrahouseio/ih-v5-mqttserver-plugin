3941e520d74749609fc5427383945ab095b4c681
nimargolis <486016@gmail.com>
2022-11-15 17:33:08 +0300

    Update for binexe

--- index.js
+++ index.js
@@ -4,13 +4,18 @@
 
 const util = require('util');
 
-const plugin = require('ih-plugin-api')();
+// const plugin = require('ih-plugin-api')();
 const app = require('./app');
 
 (async () => {
-  plugin.log('Mqtt client has started.', 0);
-
+ let plugin;
   try {
+    const opt = getOptFromArgs();
+    const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
+    plugin = require(pluginapi+'/index.js')();
+    
+    plugin.log('Mqtt client has started.', 0);
+
     // Получить параметры брокера
     plugin.params = await plugin.params.get();
     plugin.log('Received params ' + util.inspect(plugin.params));
@@ -19,3 +24,14 @@ const app = require('./app');
     plugin.exit(8, `Error: ${util.inspect(err)}`);
   }
 })();
+
+
+function getOptFromArgs() {
+  let opt;
+  try {
+    opt = JSON.parse(process.argv[2]); //
+  } catch (e) {
+    opt = {};
+  }
+  return opt;
+}
